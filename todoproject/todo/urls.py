d8964bdeed70bbc5d94d5ff8cb2dd7059cc36e5f
Darien O. Davis <dariendarien03@gmail.com>
2022-05-07 16:18:11 -0400

    Fully functioning and completed challenge

--- todoproject/todo/urls.py
+++ todoproject/todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from todo.views import TodoDetailView, TodoListView
+from todo.views import TodoDetailView, TodoListView, TodoNotesView
 
 urlpatterns = [
     path('', TodoListView.as_view(), name='todo_list'),
-    path('<int:task_id>', TodoDetailView.as_view(), name='task')
+    path('<int:task_id>', TodoDetailView.as_view(), name='task'),
+    path('todo/notes', TodoNotesView.as_view(), name='notes')
 ]
